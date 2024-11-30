69c4f84224bb66da2d8e5e2a8d916267095cb025
CodeFanaticLV <robertterekhovich@gmail.com>
2024-11-30 11:49:37 +0100

    Updated price class, added product card widget and put it in the home page.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,8 @@ import 'package:ready_to_go_store/models/Product.dart';
 import 'package:ready_to_go_store/models/app_state.dart';
 import 'package:ready_to_go_store/regular_top_bar/top_bar.dart';
 
+import 'home_page_components/product_card/product_card.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -12,7 +14,6 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<AppState>(builder: (context, state, child) =>
         Scaffold(
-
           appBar: AppBar(
             title: const TopBar(),
           ),
@@ -20,13 +21,33 @@ class HomePage extends StatelessWidget {
             future: state.products,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
+                return const Center(child: CircularProgressIndicator());
               } else if (snapshot.hasData) {
                 final products = snapshot.data!;
-                return buildProductsList(products);
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // "Deals for You" Text
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Deals for You",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    // Product Grid
+                    Expanded(
+                      child: buildProductsList(products),
+                    ),
+                  ],
+                );
               } else {
                 print(snapshot);
-                return const Text("No data available");
+                return const Center(child: Text("No data available"));
               }
             },
           ),
@@ -35,24 +56,22 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildProductsList(List<Product> posts) {
-    // ListView Builder to show data in a list
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // Number of items per row
+        crossAxisSpacing: 5, // Horizontal spacing between items
+        mainAxisSpacing: 0, // Vertical spacing between items
+      ),
       itemCount: posts.length,
+      padding: const EdgeInsets.all(5),
       itemBuilder: (context, index) {
         final product = posts[index];
-        return Container(
-          color: Colors.grey.shade300,
-          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          height: 100,
-          width: double.maxFinite,
-          child: Row(
-            children: [
-              Expanded(flex: 1, child: Image.network(product.img)),
-              SizedBox(width: 10),
-              Expanded(flex: 3, child: Text(product.title)),
-            ],
-          ),
+        return ProductCard(
+          title: product.title,
+          imageUrl: product.img,
+          price: product.price,
+          description: 'test-description',
+          onAddToCart: () {},
         );
       },
     );
--- lib/models/price.dart
+++ lib/models/price.dart
@@ -1,7 +1,11 @@
 import 'package:ready_to_go_store/models/enums/currency.dart';
 
-class Price{
+class Price {
   Price(this.currency, this.price);
   Currency currency = Currency.euro;
   double price = 0;
+
+  String toStringAsFixed(int fractionDigits) {
+    return price.toStringAsFixed(fractionDigits);
+  }
 }
